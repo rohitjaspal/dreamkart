ff4181e957a487b26ca6aef9cbd51d0525032a7f
Shiv Kumar <shivk7466@gmail.com>
2024-04-03 23:44:35 +0530

    fix: login api bug fixed

--- ui/src/components/login.tsx
+++ ui/src/components/login.tsx
@@ -4,26 +4,33 @@ import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import axios from 'axios';
 import { environment } from '../environment';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [email , setEmail] = useState('');
   const [password , setPassword] = useState('');
+  const navigate = useNavigate();
 
 
 const onsubmit = async(event:React.FormEvent) => {
+  console.log("Hiii")
+  event.preventDefault()
   console.log(email , password);
-  console.log(event);
-    const result = await axios.post(environment.baseURL + '/auth/login' , {})
-      console.log(result);
-      const {data } = result;
-      if(data && data.length > 0) localStorage.setItem('usertoken' , data.token);
-      <Navigate to ='/products' replace/>
+  try{
+    const result = await axios.post(`${environment.baseURL}/auth/login`, { email, password})
+    if(result.status === 200) {
+      localStorage.setItem('token', result.data.data.token);
+      navigate('/products')
+    }
+  }
+  catch(err){
+    console.log(err)
+  }
 }
 
   return (
     <div>
-      <form onSubmit={() => onsubmit}>
+      <form onSubmit={onsubmit}>
         <InputText type='text' value={email} onChange={(e) => setEmail(e.target.value)} className='p-inputtext-sm' placeholder='Username'>Username :</InputText>
         <InputText type='password' value={password} onChange={(e) => setPassword(e.target.value)}className='p-inputtext-sm' placeholder='Password'>Password :</InputText>
         <Button type="submit">Submit</Button>
