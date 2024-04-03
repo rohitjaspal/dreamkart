057bf7c23ede197a70a09b8bb2cb44c9fb640d86
Shiv Kumar <shivk7466@gmail.com>
2024-04-03 23:27:59 +0530

    fix: bug fixed

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -10,11 +10,7 @@ function App() {
   return (
     <div className="App">
       <PrimeReactProvider>
-        <Routess>
-        {/* <Login/>
-         <Products/>
-          <Signup/>  */}
-        </Routess>
+        <Routess />
       </PrimeReactProvider>
     </div>
   );
